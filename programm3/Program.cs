1085e3c0c505f7b2473d60867fce8da73d500417
Aleksandr <Vespin1991@bk.ru>
2022-09-01 18:55:01 +0300

    programm3

---
+++ programm3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if ( a%2==0 )
+ {Console.WriteLine("четное");}
+ else 
+ {Console.WriteLine("нечетное");}
