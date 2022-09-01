2d13bdd38f52322e1f6e7958dff8f71565eff47f
Aleksandr <Vespin1991@bk.ru>
2022-09-01 17:19:52 +0300

    programm1

---
+++ programm1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if ( a > b ) 
+{  
+    Console.WriteLine(a);
+    }
+    else {
+       Console.WriteLine ( b );
+    }
