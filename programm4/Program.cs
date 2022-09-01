e741cd5fc7c9ec11a797cdc66ff64a8850fd0dbd
Aleksandr <Vespin1991@bk.ru>
2022-09-01 20:48:42 +0300

    programm4

---
+++ programm4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+while (a <= N){
+ if ( a%2==0 )
+ { Console.WriteLine( a ); }
+ a = a+1;}
+ 
+
+
+
